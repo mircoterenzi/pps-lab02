65a31e33a2d27f441a2a187b9b316aa06ea82244
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2025-03-10 18:33:21 +0100

    add task 2

--- src/main/scala/Tasks.scala
+++ src/main/scala/Tasks.scala
@@ -33,3 +33,54 @@ object Tasks extends App :
 
   def divCurried2(a: Double)(b: Double): Double = f3(a / _)(b)
   println("divCurried2(8)(4) = " + divCurried2(8)(4))
+
+  // Task 2, Svolto da solo
+  val posLambda: Int => String =
+    case n if n >= 0 => "positive"
+    case _ => "negative"
+  def posMethod: Int => String =
+    case n if n >= 0 => "positive"
+    case _ => "negative"
+
+  // Type neg = (String => Boolean) => String => Boolean
+  val negLambda: (String => Boolean) => String => Boolean = f => x => !f(x)
+  def negMethod(f: String => Boolean): String => Boolean = x => !f(x)
+  val empty: String => Boolean = _ == ""
+  // type notEmpty = String => Boolean
+  val notEmpty = negLambda(empty)
+  println(s"notEmpty(\"foo\") = ${notEmpty("foo")}; " +
+    s"notEmpty(\"\") = ${notEmpty("")}; " +
+    s"notEmpty(\"foo\") && !notEmpty(\"\") ${notEmpty("foo") && !notEmpty("")}"
+  )
+
+  def neg[X]: (X => Boolean) => X => Boolean = f => x => !f(x)
+  val leZero: Int => Boolean = _ <= 0
+  val dog: String => Boolean = _ == "dog"
+  val gZero = neg(leZero)
+  val notDog = neg(dog)
+  println(s"gZero(1) = ${gZero(1)}; notDog(\"cat\") = ${notDog("cat")}")
+
+  val p1: Int => Int => Int => Boolean = x => y => z => x <= y && y == z
+  val p2 = (x: Int, y: Int, z: Int) => x <= y && y == z
+  def p3(x: Int)(y: Int)(z: Int): Boolean = x <= y && y == z
+  def p4(x: Int, y: Int, z: Int) = x <= y && y == z
+  println(s"input = (3,4,4): p1 = ${p1(3)(4)(4)}; p2 = ${p2(3, 4, 4)}; p3 = ${p3(3)(4)(4)}; p4 = ${p4(3, 4, 4)}")
+  println(s"input = (3,4,5): p1 = ${p1(3)(4)(5)}; p2 = ${p2(3, 4, 5)}; p3 = ${p3(3)(4)(5)}; p4 = ${p4(3, 4, 5)}")
+
+  def compose(f: Int => Int, g: Int => Int): Int => Int = x => f(g(x))
+  def composeGen[A,B,C] (f: B => C, g: A => B): A  => C = x => f(g(x))
+  // Signature composeGen = (f: B => C, g: A => B): A  => C
+  println(s"compose(_ - 1, _ * 2)(5) = ${compose(_ - 1, _ * 2)(5)}; " +
+    s"composeGen[Int](_ - 1, _ * 2)(5) = ${composeGen[Int, Int, Int](_ - 1, _ * 2)(5)}"
+  )
+  println("composeGen(_ concat \"Scala\", _ concat \",\")(\"Hello\") = " +
+    composeGen[String, String, String](_ concat "Scala", _ concat ", ")("Hello")
+  )
+
+  def composeThree[A,B,C,D](f: C => D, g: B => C, h: A => B): A => D = x => f(g(h(x)))
+  def composeForThree[A,B,C,D](f: C => D, g: B => C, h: A => B): A => D = composeGen(f, composeGen(g, h))
+  println("composeThree(_ + \"!\", _.toString, _ * 2)(3) = " +
+    composeThree[Int, Int, String, String](_ + "!", _.toString, _ * 2)(3) + "; " +
+    "composeForThree(_ + \"!\", _.toString, _ * 2)(3) = " +
+    composeForThree[Int, Int, String, String](_ + "!", _.toString, _ * 2)(3)
+  )
