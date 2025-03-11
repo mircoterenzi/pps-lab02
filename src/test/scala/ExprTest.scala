535bfbeb28e81359d7c43a586485bdfcb5c42f62
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2025-03-11 11:08:17 +0100

    improve test style

--- src/test/scala/ExprTest.scala
+++ src/test/scala/ExprTest.scala
@@ -3,58 +3,48 @@ import org.junit.*
 import org.junit.Assert.*
 
 class ExprTest:
-  @Test def testCreateLiteral(): Unit = {
+  @Test def testCreateLiteral(): Unit =
     assertNotNull(Expr.Literal(2))
-  }
 
-  @Test def testCreateAddExpr(): Unit = {
+  @Test def testCreateAddExpr(): Unit =
     val literal = Expr.Literal(2)
     assertNotNull(Expr.Add(literal, literal))
-  }
 
-  @Test def testCreateMultiplyExpr(): Unit = {
+  @Test def testCreateMultiplyExpr(): Unit =
     val literal = Expr.Literal(2)
     assertNotNull(Expr.Multiply(literal, literal))
-  }
 
-  @Test def testCreateComplexExpression(): Unit = {
+  @Test def testCreateComplexExpression(): Unit =
     val literal = Expr.Literal(2)
     assertNotNull(Expr.Multiply(Expr.Add(literal, literal), Expr.Multiply(literal, literal)))
-  }
 
-  @Test def testEvaluateLiteralReturnValue(): Unit = {
+  @Test def testEvaluateLiteralReturnValue(): Unit =
     assertEquals(2, evaluate(Expr.Literal(2)))
-  }
 
-  @Test def testEvaluateAddReturnSumOfValues(): Unit = {
+  @Test def testEvaluateAddReturnSumOfValues(): Unit =
     val firstValue = Expr.Literal(2)
     val secondValue = Expr.Literal(5)
     assertEquals(2 + 5, evaluate(Expr.Add(firstValue, secondValue)))
-  }
 
-  @Test def testEvaluateMultiplyReturnTimesValues(): Unit = {
+  @Test def testEvaluateMultiplyReturnTimesValues(): Unit =
     val firstValue = Expr.Literal(4)
     val secondValue = Expr.Literal(5)
     assertEquals(4 * 5, evaluate(Expr.Multiply(firstValue, secondValue)))
-  }
 
-  @Test def testShowLiteral(): Unit = {
+  @Test def testShowLiteral(): Unit =
     assertEquals("10", show(Expr.Literal(10)))
-  }
 
-  @Test def testShowAdd(): Unit = {
+  @Test def testShowAdd(): Unit =
     val firstValue = Expr.Literal(4)
     val secondValue = Expr.Literal(5)
     assertEquals("(4+5)", show(Expr.Add(firstValue, secondValue)))
-  }
 
-  @Test def testShowMultiply(): Unit = {
+  @Test def testShowMultiply(): Unit =
     val firstValue = Expr.Literal(4)
     val secondValue = Expr.Literal(5)
     assertEquals("(4*5)", show(Expr.Multiply(firstValue, secondValue)))
-  }
 
-  @Test def testComputeComplexExpression(): Unit = {
+  @Test def testComputeComplexExpression(): Unit =
     val expr = Expr.Add(
       Expr.Multiply(
         Expr.Multiply(
@@ -67,4 +57,3 @@ class ExprTest:
     )
     assertEquals("(((4*5)*5)+2)", show(expr))
     assertEquals(102, evaluate(expr))
-  }
